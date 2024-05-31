325bd63d701808de3d922ebbe5f63234e65efc65
Beltz <humberto.c.lima18@gmail.com>
2024-05-31 10:01:36 -0300

    Add `.gitignore` and basic spring config files.

---
+++ src/main/java/com/app/smartPocket/SmartPocketApplication.java
@@ -0,0 +1,13 @@
+package com.app.smartPocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/smartPocket/SmartPocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.smartPocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
